2373d7c5fef5f3f98daa68eabb3fb8a3b4f968b0
Sarah Kuhn <kuhnsa@student.ethz.ch>
2025-04-30 18:36:50 +0100

    figure out why my is dffrent from main

--- LeanRV64DLEAN/Sail/Sail.lean
+++ LeanRV64DLEAN/Sail/Sail.lean
@@ -153,7 +153,7 @@ def toHexUpper (i : Int) : String :=
   | Int.negSucc n => "-" ++ Nat.toHexUpper (n+1)
 
 end Int
--- def get_slice_int (len n lo : Nat) (n : Int) : BitVec len :=
+
 def get_slice_int (len lo : Nat) (n : Int) : BitVec len :=
   BitVec.extractLsb' lo len (BitVec.ofInt (lo + len + 1) n)
 
